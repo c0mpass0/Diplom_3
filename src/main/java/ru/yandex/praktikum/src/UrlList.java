0704c7febe3bdb81336f22506ff63ebda5da46f1
Bezoru <c0mpass0max@gmail.com>
2023-04-11 17:50:08 +0700

    Fix concern - update uri list

--- src/main/java/ru/yandex/praktikum/src/UrlList.java
+++ src/main/java/ru/yandex/praktikum/src/UrlList.java
@@ -2,9 +2,9 @@ package ru.yandex.praktikum.src;
 
 public class UrlList {
     public static final String MAIN_PAGE_URL = "https://stellarburgers.nomoreparties.site/";
-    public static final String ACCOUNT_PAGE_URL = "https://stellarburgers.nomoreparties.site/login";
-    public static final String REGISTRATION_PAGE_URL = "https://stellarburgers.nomoreparties.site/register";
-    public static final String PASSWORD_RECOVERY_PAGE_URL = "https://stellarburgers.nomoreparties.site/forgot-password";
-    public static final String CABINET_PAGE_URL = "https://stellarburgers.nomoreparties.site/account/profile";
+    public static final String ACCOUNT_PAGE_URL = MAIN_PAGE_URL + "login";
+    public static final String REGISTRATION_PAGE_URL = MAIN_PAGE_URL + "register";
+    public static final String PASSWORD_RECOVERY_PAGE_URL = MAIN_PAGE_URL + "forgot-password";
+    public static final String CABINET_PAGE_URL = MAIN_PAGE_URL + "account/profile";
 
 }
